e926ea2ee514b692b937431617bdd5476afbb79b
fu.xiao <im.fu.xiao@gmail.com>
2023-01-04 21:34:44 +0800

    chore: swift package manager support

--- Package.swift
+++ Package.swift
@@ -24,10 +24,12 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "Plist",
-      dependencies: [],
-      resources: [.copy("Resources")]),
+      dependencies: []
+    ),
     .testTarget(
       name: "PlistTests",
-      dependencies: ["Plist"]),
+      dependencies: ["Plist"],
+      resources: [.copy("Resources")]
+    ),
   ]
 )
